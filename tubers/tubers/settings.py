da205ccc07c97362a25ceb23cb66ba1c8678e1c5
Jithesh944 <43859372+jithesh944@users.noreply.github.com>
2021-07-11 22:00:06 +0530

    updated

--- tubers/tubers/settings.py
+++ tubers/tubers/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^ktwjdn#%m*#=d37wddnef5e2h1i$baixecydjkp5*+o83f0pz'
+SECRET_KEY = '^ktwjdn#%m*#=d37wddnfgbef5e2h1i$baixecydjkp5*+o83f0pz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
